a1376efdadb162019a513a65963f4639fd3047c7
HRC-2K <1KINGHR@gmail.com>
2026-07-26 07:29:09 +0600

    Update Steam_Ubi_Epic_RStar.ps1

--- Steam_Ubi_Epic_RStar.ps1
+++ Steam_Ubi_Epic_RStar.ps1
@@ -1,5 +1,5 @@
 <#
-    Steam_Ubi_Epic_RStar.ps1 - Multi-Launcher Firewall Blocker (Powershell)
+    Steam_Ubi_Epic_RStar.ps1 - Multi-Launcher Firewall Blocker (Powershell Interactive Menu)
     Copyright (C) 2026 HRC-2K <https://github.com/HRC-2K/OFFLINE_ACTIVATION>
 
     This program is free software: you can redistribute it and/or modify
