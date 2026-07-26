3575c2b3f5c4dedb60a558b211f21443ba2185cd
HRC-2K <1KINGHR@gmail.com>
2026-07-26 06:52:04 +0600

    Update EAOMG.ps1

--- EAOMG.ps1
+++ EAOMG.ps1
@@ -1,3 +1,22 @@
+<#
+    EAOMG.ps1 - EA Hardware Isolation & Offline Adapter Tool
+    Copyright (C) 2026 HRC-2K <https://github.com/HRC-2K/OFFLINE_ACTIVATION>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#>
+
+
 # ==============================================================================
 # 1. AUTOMATIC ADMINISTRATOR ELEVATION
 # ==============================================================================
